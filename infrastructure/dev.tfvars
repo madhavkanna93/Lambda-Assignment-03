9e4538ccca856924358bb328f1933b93124d2906
madhavkanna93 <109338181+madhavkanna93@users.noreply.github.com>
2022-07-26 00:38:24 +0530

    update lambda

---
+++ infrastructure/dev.tfvars
@@ -0,0 +1,3 @@
+deployment_bucket_name = "psi-lambda-function"
+deployment_path_key = "production"
+deployment_package_name = "lambda_function"
---
+++ infrastructure/modules/lambda_deployment/lambda.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+data "aws_s3_bucket_object" "jar_hash" {
+  bucket = var.deployment_bucket_name
+  key    = "${var.deployment_path_key}/${var.deployment_package_name}.zip"
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  function_name = "my-sample-lambda"
+  s3_bucket        = var.deployment_bucket_name
+  s3_key = "${var.deployment_path_key}/${var.deployment_package_name}.zip"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda_function.lambda_handler"
+
+  # source_code_hash = filebase64sha256("${var.deployment_path_key}/${var.deployment_package_name}.zip")
+
+  runtime = "python3.8"
+}
